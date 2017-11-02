0370dc3fb19e15b5bb65faa24f39d35fdab6530d
Jefferson Reis <jefferson.reis@cappta.com.br>
2017-11-02 20:50:00 -0200

    teste com for

--- maior_ou_menot.rb
+++ maior_ou_menot.rb
@@ -12,19 +12,24 @@ numero_secreto = 175
 puts "Escolhido... que tal advinhar hoje nosso número secreto?"
 puts "\n\n\n\n"
 
-puts "Tentativa 1"
-puts "Entre com o número"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
+limite_de_tentativas = 5
+for tentativa in 1..limite_de_tentativas
+	puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+	puts "Entre com o número"
+	chute = gets
+	puts "Será que acertou? Você chutou " + chute
 
-acertou = numero_secreto == chute.to_i
-if acertou
-	puts "Acertou!"
-else
-	maior = numero_secreto > chute.to_i
-	if maior
-		puts "Errou! O Número secreto é maior"
+	acertou = numero_secreto == chute.to_i
+	if acertou
+		puts "Acertou!"
+		break
 	else
-		puts "Errou! O Número secreto é menor"
+		maior = numero_secreto > chute.to_i
+		if maior
+			puts "Errou! O Número secreto é maior"
+		else
+			puts "Errou! O Número secreto é menor"
+		end
 	end
+puts "\n\n"
 end
