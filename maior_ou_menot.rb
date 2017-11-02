6e7bb9a5cb76128a7bfb65bf0945b7bcb797de7d
Jefferson Reis <jefferson.reis@cappta.com.br>
2017-11-02 20:01:04 -0200

    ...

--- maior_ou_menot.rb
+++ maior_ou_menot.rb
@@ -1,19 +1,24 @@
 puts "Seja Bem vindo ao Jogo da advinhação"
 puts "Qual é o seu nome?"
 nome = gets
-puts "\n\n\n\n\n\n"
+puts "\n\n\n\n"
+
 puts "Começaremos o jogo para você, " + nome
-puts "\n\n\n\n\n\n"
+puts "\n\n\n\n"
+
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
+
 puts "Escolhido... que tal advinhar hoje nosso número secreto?"
 puts "\n\n\n\n"
+
 puts "Tentativa 1"
 puts "Entre com o número"
 chute = gets
 puts "Será que acertou? Você chutou " + chute
 
-if (chute.to_i == numero_secreto)
+acertou = numero_secreto == chute.to_i
+if acertou
 	puts "Acertou!"
 else
 	puts "Errou!"
