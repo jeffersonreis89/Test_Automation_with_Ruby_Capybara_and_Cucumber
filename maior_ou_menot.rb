359e54690398772c4f85e3378b649364561b8599
Jefferson Reis <jefferson.reis@cappta.com.br>
2017-11-02 18:42:04 -0200

    inserido quebra linha

--- maior_ou_menot.rb
+++ maior_ou_menot.rb
@@ -1,18 +1,13 @@
 puts "Seja Bem vindo ao Jogo da advinhação"
 puts "Qual é o seu nome?"
 nome = gets
-puts
-puts
-puts
+puts "\n\n\n\n\n\n"
 puts "Começaremos o jogo para você, " + nome
-puts
+puts "\n\n\n\n\n\n"
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal advinhar hoje nosso número secreto?"
-puts
-puts
-puts
-puts
+puts "\n\n\n\n"
 puts "Tentativa 1"
 puts "Entre com o número"
 chute = gets
