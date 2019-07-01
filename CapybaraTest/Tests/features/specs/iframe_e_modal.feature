b21b13612b507f0a00f71e24733b6f5f5154dff5
Jefferson <jeffersonreissa@gmail.com>
2019-07-01 18:01:59 -0300

    added new tests with iframe and modal

---
+++ CapybaraTest/Tests/features/specs/iframe_e_modal.feature
@@ -0,0 +1,10 @@
+#language: pt
+
+@iframe_e_modal
+Funcionalidade: Utilizando Iframe e Modal
+
+Cenario: Usando iframe
+Quando entro no iframe e preencho os campos
+
+Cenario: Usando modal
+Quando entro no modal e verifico o texto
---
+++ CapybaraTest/Tests/features/step_definitions/iframe_e_modal.rb
@@ -0,0 +1,21 @@
+Quando("entro no iframe e preencho os campos") do
+    visit '/mudancadefoco/iframe'
+    
+    #mudanca para o iframe
+    within_frame('id_do_iframe') do
+        fill_in(id: 'first_name', with: 'Jeff')
+        fill_in(id: 'last_name', with: 'Reis')
+        sleep(3)
+    end
+  end
+  
+  Quando("entro no modal e verifico o texto") do
+    visit '/mudancadefoco/modal'
+    find('a[href="#modal1"]').click
+
+    within('#modal1')do
+    texto = find('h4')
+    expect(texto.text).to eql 'Modal Teste'
+    find('.modal-close').click
+    end
+  end
