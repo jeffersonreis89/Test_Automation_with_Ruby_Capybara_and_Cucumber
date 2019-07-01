0523dbe5aa6248db5f53bd877eac341ef26e20e6
Jefferson <jeffersonreissa@gmail.com>
2019-07-01 18:01:38 -0300

    added new test with asserts

---
+++ CapybaraTest/Tests/features/step_definitions/verificando_elementos_visiveis.rb
@@ -0,0 +1,18 @@
+Quando("clico no botao") do
+    visit '/buscaelementos/botoes'
+    find('#teste').click
+  end
+  
+  Entao("verifico se o texto apareceu na tela com sucesso") do
+    # texto = find('#div1')
+    # expect(texto.text).to eql 'Você Clicou no Botão!'
+
+    page.assert_text(text, 'Você Clicou no Botão!')
+    page.has_text?('Você Clicou no Botão!')
+    have_text('Você Clicou no Botão!')
+
+    find('#teste').click
+    sleep(2)
+    assert_no_text(text, 'Você Clicou no Botão!')
+    has_no_text?('Você Clicou no Botão')
+  end
